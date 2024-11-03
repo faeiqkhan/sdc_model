f56b8698c3693ffa4d534f87b251b0474aa7d516
faeiqkhan <khanfaeiq@gmail.com>
2024-11-03 23:49:21 +0530

    Behavioral cloning complete

--- Behaviorall_Cloning/Model_test/drive.py
+++ Behaviorall_Cloning/Model_test/drive.py
@@ -1,53 +1,52 @@
 import socketio
 import eventlet
+import numpy as np
 from flask import Flask
 from keras.models import load_model
 import base64
-import cv2
-import numpy as np
 from io import BytesIO
 from PIL import Image
+import cv2
+
 
 sio = socketio.Server()
+app = Flask(__name__)
 
-app = Flask(__name__)#'--main__
 
+SPEED_LIMIT = 40
 
-def img_preprocess(img):
-    img = img[60:135,:,:]
+def preprocess_image(img):
+    img = img[60:135, :, :]
     img = cv2.cvtColor(img, cv2.COLOR_RGB2YUV)
-    img = cv2.GaussianBlur(img,  (3, 3), 0)
+    img = cv2.GaussianBlur(img, (3, 3), 0)
     img = cv2.resize(img, (200, 66))
-    img = img/255
+    img = img / 255.0
     return img
-
 
 @sio.on('telemetry')
 def telemetry(sid, data):
+    speed = float(data['speed'])
     image = Image.open(BytesIO(base64.b64decode(data['image'])))
     image = np.asarray(image)
-    image = img_preprocess(image)
+    image = preprocess_image(image)
     image = np.array([image])
     steering_angle = float(model.predict(image))
-    send_control(steering_angle, 1.0)
+    throttle = 1.0 - speed / SPEED_LIMIT
+    print(f'Steering Angle: {steering_angle}, Throttle: {throttle}, Speed: {speed}')
+    send_control(steering_angle, throttle)
 
 @sio.on('connect')
 def connect(sid, environ):
-    print('connected')
+    print("Simulator connected")
     send_control(0, 0)
-    
-    
+
 def send_control(steering_angle, throttle):
-    sio.emit ('steer', data = {
-        'steering_angle': steering_angle.__str__(),
-        'throttle': throttle.__str__()
-    })   
-        
-
-
-
+    sio.emit('steer', data={
+        'steering_angle': str(steering_angle),
+        'throttle': str(throttle)
+    })
 
 if __name__ == '__main__':
-    model = load_model('model.h5')
-    app = socketio.Middleware(sio, app)
+    model = load_model('/home/anonymous/Desktop/sdc_model/Behaviorall_Cloning/Model_test/model.h5', compile=False)
+    app = socketio.WSGIApp(sio, app)
     eventlet.wsgi.server(eventlet.listen(('', 4567)), app)
